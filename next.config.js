1b239d4c9e27a44cae6ebcbdd128df4be268b42f
Jacobtread <jacobtread@gmail.com>
2022-07-10 23:53:52 +1200

    Disabled image opt x2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true
+  experimental: {
+    images: {
+      unoptimized: true
+    }
   }
 }
 
