85871ff109868013fbc7a9309bf69b3dfd7d9c3d
Jacobtread <jacobtread@gmail.com>
2022-07-15 19:18:52 +1200

    Changed up about text

--- components/About.tsx
+++ components/About.tsx
@@ -11,11 +11,11 @@ const About: FunctionComponent = () => {
                         History
                     </h2>
                     <p className={styles.block__text}>
-                        New Zealand is where I was born and so far i’ve spent my entire life here. From the ripe young
-                        age of 11
-                        I’ve always had a great interest in programming and have spent a majority of my time learning
-                        and building up my skills with different programming languages, frameworks, and toolsets. My
-                        earliest programming adventures came about from the early days of Minecraft modding.
+                        New Zealand is where I was born and so far I’ve spent my entire life here. From the ripe young
+                        age of 11, I’ve always had a great interest in programming and have spent a majority of my time
+                        learning and building up my skills with different programming languages, frameworks, and
+                        toolsets. My earliest programming adventures came about from the early days of Minecraft
+                        modding.
                     </p>
                 </div>
                 <div className={styles.block} data-block="interests">
@@ -23,12 +23,10 @@ const About: FunctionComponent = () => {
                         Interests
                     </h2>
                     <p className={styles.block__text}>
-                        I am very interested in learning programming related subjects as I have spent almost of all my
+                        I am very interested in learning topics relating to programming. I have spent almost of all my
                         education career following the programming pathways. I quite enjoy working with other peoples
-                        code
-                        through reverse engineering and other collaboration. Outside of programming I am also interested
-                        in
-                        body building and I like to keep a strict routine with my exercise.
+                        code through reverse engineering and other collaboration. Outside of programming I am also
+                        interested in body building and I like to keep a strict routine with my exercise.
                     </p>
                 </div>
                 <div className={styles.block + " " + styles['block--span']} data-block="projects">
@@ -36,13 +34,10 @@ const About: FunctionComponent = () => {
                         Projects
                     </h2>
                     <p className={styles.block__text}>
-                        I’ve worked on many projects throughout the years many of which are readily available on my
-                        Github
-                        the projects listed on my github are majority under the MIT license. My projects come in all
-                        kinds
-                        of varieties in all different kinds of languages and frameworks. Feel free to take a look around
-                        and
-                        see if any of them spark your interests
+                        I’ve worked on many projects throughout the years, many of which are readily available on my
+                        Github the projects listed on my github are majority under the MIT license. My projects come in
+                        all kinds of varieties in all different kinds of languages and frameworks. Feel free to take a
+                        look around and see if any of them spark your interests
                     </p>
                 </div>
 
