8a86c1072412568c9767d1d572e15266df7b8f3a
Jacobtread <jacobtread@gmail.com>
2022-07-17 14:34:21 +1200

    added page meta and icons and moved images to image folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,32 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 function MyApp({Component, pageProps}: AppProps) {
+    const description: string = "This website is my personal / portfolio website. It contains information about me, my contact information, and a list of projects."
+
     return (
         <div>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="Jacobtread | Full-stack Developer"/>
+                <meta property="og:description" content={description}/>
+                <meta property="og:url" content="https://jacobtread.com"/>
+                <meta property="og:image" content="/img/jacobtread.png"/>
+
+                <meta name="author" content="Jacobtread"/>
+                <meta name="description" content={description}/>
+                <meta name="keywords" content="developer,jacobtread,jacob,programming,software,fullstack,web"/>
+                <meta name="theme-color" color="#7ed380"/>
+
+                <title>Jacobtread | Full-stack Developer</title>
+            </Head>
             <Component {...pageProps} />
             <Footer/>
         </div>
