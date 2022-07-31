ae82d1eff99f724ea0516e2f3d98b2d8b14ad22d
Jacobtread <jacobtread@gmail.com>
2022-07-31 19:30:21 +1200

    Adjusted config for export

--- next.config.js
+++ next.config.js
@@ -3,9 +3,12 @@ const nextConfig = {
     reactStrictMode: true,
     experimental: {
         images: {
+            // Images cannot be optimized when SSG is done
             unoptimized: true
         }
-    }
+    },
+    // Trailing slashes to make webserver work properly with SSG
+    trailingSlash: true
 }
 
 module.exports = nextConfig
