b565f8a714f473935ff74df2297919b811a7d2de
Jacobtread <jacobtread@gmail.com>
2022-09-26 18:54:25 +1300

    Fixed missing html lang and i18 not compatible with export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        locales: ["en"],
-        defaultLocale: "en"
-    },
     reactStrictMode: true,
     experimental: {
         images: {
